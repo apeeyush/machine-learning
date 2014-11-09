f20720be00c8ea542e4fd545f8da038cabc6a84f
peeyush <peeyushagarwal1994@gmail.com>
2014-11-09 07:58:53 +0530

    Add binary moview review classification python example

---
+++ sentiment-analysis-on-movie-review/code/train.py
@@ -0,0 +1,25 @@
+# TEXT CLASSIFICATION FOR SENTIMENT ANALYSIS – NAIVE BAYES CLASSIFIER
+# Bag of Words Feature Extraction
+import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import movie_reviews
+
+def word_feats(words):
+    return dict([(word, True) for word in words])
+
+negids = movie_reviews.fileids('neg')
+posids = movie_reviews.fileids('pos')
+
+negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
+posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
+
+negcutoff = len(negfeats)*3/4
+poscutoff = len(posfeats)*3/4
+
+trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
+testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
+print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
+
+classifier = NaiveBayesClassifier.train(trainfeats)
+print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
+classifier.show_most_informative_features()
